dfe00b862010bce296f0fdbbb0478f97684a5164
Abner A Oliveira <abneralves10@hotmail.com>
2020-01-23 18:01:09 -0300

    Added card-note component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CardNoteComponent} from "./components/card-note/card-note.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    RouterModule.forRoot([
+      { path: '', component: CardNoteComponent },
+    ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardNoteComponent } from './components/card-note/card-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardNoteComponent
   ],
   imports: [
     BrowserModule,
